01ea4370ba2002b41048b7b6854e6b5e2e49f9af
ravindersingh74123 <ravindersingh74123@gmail.com>
2025-07-06 17:45:03 +0530

    Fixed db folder: removed submodule and added as normal folder

---
+++ db/dist/common/language/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LANGUAGE_MAPPING = void 0;
+exports.LANGUAGE_MAPPING = {
+    js: { judge0: 63, internal: 1, name: "Javascript", monaco: "javascript" },
+    cpp: { judge0: 54, internal: 2, name: "C++", monaco: "cpp" },
+    rs: { judge0: 73, internal: 3, name: "Rust", monaco: "rust" },
+    java: { judge0: 62, internal: 4, name: "Java", monaco: "java" },
+};
