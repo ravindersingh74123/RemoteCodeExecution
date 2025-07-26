e8554f5c8c0cb592dce51b63e7fe10f5fbc2f528
ravindersingh74123 <ravindersingh74123@gmail.com>
2025-07-26 15:04:05 +0530

    updated lint for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@/ui/src"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
